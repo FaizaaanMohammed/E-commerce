5b6590ccb73b359cbbc5ab3574ac70909a81dcda
Faizan Mohammed <faizan.desuntechnology@gmail.com>
2026-06-19 21:28:51 +0530

    add AdminAuth minddleware and register email verification and login is completed

--- Backend/app/middlewares/AdminAuthCheck.js
+++ Backend/app/middlewares/AdminAuthCheck.js
@@ -1,36 +1,18 @@
-const jwt = require("jsonwebtoken");
-const httpStausCode = require("../utils/httpsStatusCode");
+const httpStausCode = require("../utils/httpsStatusCode")
 
 
-
-const adminAuthCheck = (req,res,next)=>{
-  
-    const token = req?.body?.token || req?.query?.token || req?.headers?.['x-access-token'] || req?.headers?.['authorization']
-
-    if(!token){
-        return res.status(httpStausCode.BAD_REQUEST).json({
-           status:false,
-           message:"Token Is Required For Acess this url"
+const AdminAuthcheck = (req,res,next)=>{
+    try{
+      if(req.user && req.user.role === "admin"){
+        next()
+      }
+    }
+    catch(err){
+        return res.status(httpStausCode.FORBIDDEN).json({
+            success:false,
+            message:"You are not authorize for this operation"
         })
     }
-
-    if(token.startsWith('Bearer ')){
-        token = token.split(' ')[1];
-    }
-
-     try{
-        const decoded = jwt.verify(token , process.env.JWT_SECRET);
-        req.admin = decoded
-     }
-     catch(err){
-        return res.status(httpStausCode.BAD_REQUEST).json({
-          status:false,
-          message:"Invalid Token"
-        })
-     }
-
-     return next();
-
 }
 
-module.exports = adminAuthCheck
+module.exports = AdminAuthcheck
--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,6 @@
-
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']) // Forces fallback to Google's public DNS
+dns.setDefaultResultOrder('ipv4first');
 const dotenv = require("dotenv");
 const express = require("express");
 const ConnectDb = require('./app/config/db');
