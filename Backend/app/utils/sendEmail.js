edc4f7548b7374c844b244d1d841e6aedc094cfc
Faizan Mohammed <faizan.desuntechnology@gmail.com>
2026-06-19 18:19:16 +0530

    add

--- Backend/app/utils/sendEmail.js
+++ Backend/app/utils/sendEmail.js
@@ -4,6 +4,9 @@ const sendEmail = async(option) => {
   // Transporter
 
   const Transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // 🟩 Direct host define karo Gmail ka
+    port: 465,              // 🟩 Secure port
+    secure: true,
     service: "Gmail",
     auth: {
       user: process.env.EMAIL_USER,
