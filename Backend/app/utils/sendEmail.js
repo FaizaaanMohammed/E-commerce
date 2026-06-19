936a75a9cbbea5b3e02e5c54dedbfaaae318f887
Faizan Mohammed <faizan.desuntechnology@gmail.com>
2026-06-19 20:56:03 +0530

    add resend

--- Backend/app/utils/sendEmail.js
+++ Backend/app/utils/sendEmail.js
@@ -1,12 +1,12 @@
 const { Resend } = require("resend");
 
 const sendEmail = async (option) => {
-  // 🟩 .env se API Key load hogi, koi hardcoding nahi
+ 
   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  // Email bhejte hain API ke through (Port 443 - jo Render block nahi kar sakta)
+ 
   await resend.emails.send({
-    from: "onboarding@resend.dev", // Resend ka default free testing sender email
+    from: "Md Faizan <onboarding@resend.dev>", 
     to: option.email,
     subject: option.subject,
     html: option.html,
